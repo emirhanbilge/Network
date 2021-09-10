89f32bfca142b065f6108b5d3cecade6f835fb6b
Emirhan Bilge Bulut <36032629+emirhanbilge@users.noreply.github.com>
2021-09-10 16:06:28 +0300

    Add files via upload

--- modbusCrontabCreate.sh
+++ modbusCrontabCreate.sh
@@ -1,8 +1,8 @@
 echo 1234 | sudo -S su
-sudo echo "cd /home/pi/EbbScripts/ModbusScd" >> running.sh
+sudo echo "cd /home/pi/Desktop/EbbScripts/ModbusScd" >> running.sh
 sudo echo "sudo python3 main.py" >> running.sh
 sudo crontab -l > crontab_new2
-sudo echo "@reboot /home/pi/EbbScripts/Network/running.sh" >> crontab_new2
+sudo echo "@reboot /home/pi/Desktop/EbbScripts/Network/running.sh" >> crontab_new2
 sudo crontab crontab_new2
 sudo rm crontab_new2
 sudo chmod +x /home/pi/Desktop/EbbScripts/Network/running.sh
@@ -11,13 +11,13 @@ sudo chmod +x /home/pi/Desktop/EbbScripts/Network/running.sh
 ################ Normal webserverimiz için ##################
 echo 1234 | sudo -S su
 
-sudo echo "cd /home/pi/EbbScripts/Network/WebServer" >> serverRunner.sh
+sudo echo "cd /home/pi/Desktop/EbbScripts/Network/WebServer" >> serverRunner.sh
 sudo echo "sudo python3 backend.py" >> serverRunner.sh
 sudo crontab -l > crontab_new2
-sudo echo "@reboot /home/pi/EbbScripts/Network/serverRunner.sh" >> crontab_new2
+sudo echo "@reboot /home/pi/Desktop/EbbScripts/Network/serverRunner.sh" >> crontab_new2
 sudo crontab crontab_new2
 sudo rm crontab_new2
-sudo chmod +x /home/pi/Desktop/EbbScripts/Network/running.sh
+sudo chmod +x /home/pi/Desktop/EbbScripts/Network/serverRunner.sh
 ###########################################################
 
 
